f5cd822d6bb94c6b01d8abccb71850d28e7834f3
Junho Heo <hanascreen@konkuk.ac.kr>
2025-11-08 02:01:50 +0900

    fix: 필요없는 코드 일부 삭제 및 패키지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
 //Libarary or styles import
-import React, { useState, useRef, useEffect } from 'react';
-import { NavigationContainer, DefaultTheme, useNavigationContainerRef } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text, Button, SafeAreaView, ScrollView, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import moment from 'moment';
-import { styles } from './styles/styles';
-import { MaterialCommunityIcons as Icon, MaterialCommunityIcons } from '@expo/vector-icons';
-import { SafeAreaProvider } from 'react-native-safe-area-context'; //iOS M자 탈모 대비
+import React, { useState, useRef, useEffect } from "react";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  View,
+  Text,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+} from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import moment from "moment";
+import { styles } from "./styles/styles";
+import { MaterialCommunityIcons as Icon, MaterialCommunityIcons } from "@expo/vector-icons";
+import { SafeAreaProvider } from "react-native-safe-area-context"; //iOS M자 탈모 대비
 
 //Redux 라이브러리를 사용하기 위해 최상위 컴포넌트에 연결해야 한다. <Provider>와 store.js를 import한다.
-import { Provider } from 'react-redux';
-import store from './store'
+import { Provider } from "react-redux";
+import store from "./store";
 
-import { setNavigationRef } from './apis/customAxios'; // customAxios.tsx에서 가져온 navigation 설정 함수
-
+import { setNavigationRef } from "./apis/customAxios"; // customAxios.tsx에서 가져온 navigation 설정 함수
 
 //components(Screen들이라고 생각하면 됩니다) import
-import SplashScreen from '@screens/SplashScreen';
-import MainScreen from '@screens/MainScreen';
-import TextInputScreen from '@screens/TextInputScreen';
-import DetailScreen from '@screens/DetailScreen';
-import MyPageScreen from '@screens/MyPageScreen';
-import LoginScreen from '@screens/LoginScreen';
-import SettingScreen from '@screens/SettingScreen';
-import ActivityLevelFixScreen from '@screens/ActivityLevelFixScreen';
-import FixBasicDataScreen from '@screens/FixBasicDataScreen';
-import FixTextInputScreen from '@screens/FixTextInputScreen';
-
+import SplashScreen from "@screens/SplashScreen";
+import MainScreen from "@screens/MainScreen";
+import TextInputScreen from "@screens/TextInputScreen";
+import DetailScreen from "@screens/DetailScreen";
+import MyPageScreen from "@screens/MyPageScreen";
+import LoginScreen from "@screens/LoginScreen";
+import SettingScreen from "@screens/SettingScreen";
+import ActivityLevelFixScreen from "@screens/ActivityLevelFixScreen";
+import FixBasicDataScreen from "@screens/FixBasicDataScreen";
+import FixTextInputScreen from "@screens/FixTextInputScreen";
 
 //Tab으로 오고 갈 수 있는 3가지 화면을 정의한다 (MaterialBottomTabNavigator를 사용)
 let Tab = createBottomTabNavigator();
@@ -40,44 +51,45 @@ function TabNavigator() {
       initialRouteName="Main"
       screenOptions={{
         headerShown: false, //모든 탭의 header를 숨기기 위해서 사용
-        tabBarActiveTintColor: '#477C1E', // Active 상태의 아이콘 색상
-        tabBarInactiveTintColor: '#000000', // Inactive 상태의 아이콘 색상
+        tabBarActiveTintColor: "#477C1E", // Active 상태의 아이콘 색상
+        tabBarInactiveTintColor: "#000000", // Inactive 상태의 아이콘 색상
         tabBarShowLabel: false, // Label을 보여줄지 여부
         tabBarStyle: {
           height: 76,
-          backgroundColor: '#FFFFFF', // 전체 배경색 설정
+          backgroundColor: "#FFFFFF", // 전체 배경색 설정
           elevation: 0, //안드로이드에서 TabBar의 그림자 제거
           shadowOpacity: 0, //iOS에서 TabBar의 그림자 제거
         },
-      }}>
-      <Tab.Screen 
-        name="Main" 
+      }}
+    >
+      <Tab.Screen
+        name="Main"
         component={MainScreen}
         options={{
-          tabBarLabel: 'Main',
-          tabBarIcon: ({color}) => (
-            <MaterialCommunityIcons name="home" color={color} size={36} />
-          ),
+          tabBarLabel: "Main",
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="home" color={color} size={36} />,
         }}
-        />
-      <Tab.Screen 
-        name="Details" 
+      />
+      <Tab.Screen
+        name="Details"
         component={DetailScreen}
         options={{
-          tabBarLabel: 'Details',
-          tabBarIcon: ({color}) => (
+          tabBarLabel: "Details",
+          tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="chart-box-outline" color={color} size={36} />
           ),
-        }}/>
-      <Tab.Screen 
-        name="MyPage" 
+        }}
+      />
+      <Tab.Screen
+        name="MyPage"
         component={MyPageScreen}
         options={{
-          tabBarLabel: 'MyPage',
-          tabBarIcon: ({color}) => (
+          tabBarLabel: "MyPage",
+          tabBarIcon: ({ color }) => (
             <MaterialCommunityIcons name="account" color={color} size={36} />
           ),
-        }}/>
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -87,17 +99,21 @@ let Stack = createNativeStackNavigator();
 
 function StackNavigation() {
   console.log("Stack Navigation Rendering");
-  
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false}} />
-      <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} /> 
-      <Stack.Screen name="SettingScreen" component={SettingScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="SettingScreen"
+        component={SettingScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="TabNavigator" component={TabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="TextInputScreen" component={TextInputScreen}/>
+      <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
       <Stack.Screen name="ActivityLevelFixScreen" component={ActivityLevelFixScreen} />
-      <Stack.Screen name="FixBasicDataScreen" component={FixBasicDataScreen}/>
-      <Stack.Screen name="FixTextInputScreen" component={FixTextInputScreen}/>
+      <Stack.Screen name="FixBasicDataScreen" component={FixBasicDataScreen} />
+      <Stack.Screen name="FixTextInputScreen" component={FixTextInputScreen} />
     </Stack.Navigator>
   );
 }
@@ -107,32 +123,33 @@ const navTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: '#FFFFFF', //흰색으로 설정한다
+    background: "#FFFFFF", //흰색으로 설정한다
   },
 };
 
 //최상위 App Component
 function App(props: any) {
-
   const navigationRef = useNavigationContainerRef(); //Navigation 참조 생성
 
   //렌더링 시 최초 1회만 설정
   useEffect(() => {
     if (navigationRef) {
       setNavigationRef(navigationRef);
-      console.log('navigationRef 초기화 완료:', navigationRef);
+      console.log("navigationRef 초기화 완료:", navigationRef);
     }
   }, [navigationRef]);
 
-  {/* SafeAreaView로 감싸서 상단 StatusBar를 고려한다 (아이폰 M자 탈모 대비)*/}
+  {
+    /* SafeAreaView로 감싸서 상단 StatusBar를 고려한다 (아이폰 M자 탈모 대비)*/
+  }
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         {/* SafeAreaProvider로 감싸줌*/}
-        <SafeAreaProvider> 
+        <SafeAreaProvider>
           <SafeAreaView style={styles.container}>
             <NavigationContainer theme={navTheme}>
-              <StackNavigation/>
+              <StackNavigation />
             </NavigationContainer>
           </SafeAreaView>
         </SafeAreaProvider>
--- src/slices/accountInfoSlice.ts
+++ src/slices/accountInfoSlice.ts
@@ -1,100 +1,99 @@
 //redux-toolkit 사용할 것임
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 //회원가입 정보
 interface accountInfo {
-    email: string | null,
-    name: string | null,
-    height: number | null,
-    weight: number | null,
-    age: number | null,
-    gender: string | null,
-    activityLevel: string | null; //LOW, LIGHT, MEDIUM, HIGH중 하나
-    bmr: number | null; //기초대사량(BMR)
-    activityMetabolism: number | null; //활동대사량
+  email: string | null;
+  name: string | null;
+  height: number | null;
+  weight: number | null;
+  age: number | null;
+  gender: string | null;
+  activityLevel: string | null; //LOW, LIGHT, MEDIUM, HIGH중 하나
+  bmr: number | null; //기초대사량(BMR)
+  activityMetabolism: number | null; //활동대사량
 }
 
 const accountInitialState: accountInfo = {
-    email: null,
-    name: null,
-    height: null,
-    weight: null,
-    age: null,
-    gender: null,
-    activityLevel: null,
-    bmr: null,
-    activityMetabolism: null
-}
+  email: null,
+  name: null,
+  height: null,
+  weight: null,
+  age: null,
+  gender: null,
+  activityLevel: null,
+  bmr: null,
+  activityMetabolism: null,
+};
 
 let accountInfoSlice = createSlice({
-    name: 'accountInfo',
-    initialState: accountInitialState,
-    reducers: {
-        setEmail: (state, action: PayloadAction<string | null>) => {
-            state.email = action.payload;
-        },
-        setName: (state, action: PayloadAction<string | null>) => {
-            state.name = action.payload;
-        },
-        setHeight: (state, action: PayloadAction<number | null>) => {
-            state.height = action.payload;
-        },
-        setWeight: (state, action: PayloadAction<number | null>) => {
-            state.weight = action.payload;
-        },
-        setAge: (state, action: PayloadAction<number | null>) => {
-            state.age = action.payload;
-        },
-        setGender: (state, action: PayloadAction<string | null>) => {
-            state.gender = action.payload;
-        },
-        setActivityLevel: (state, action: PayloadAction<string | null>) => {
-            state.activityLevel = action.payload;
-        },
-        calculateBMR: (state) => {
-            if (state.height && state.weight && state.age && state.gender) {
-                if (state.gender === 'MALE') {
-                    state.bmr = 88.362 + (13.397 * state.weight) + (4.799 * state.height) - (5.677 * state.age);
-                } else if (state.gender === 'FEMALE') {
-                    state.bmr = 447.593 + (9.247 * state.weight) + (3.098 * state.height) - (4.330 * state.age);
-                }
-            }
-        },
-        calculateActivityMetabolism: (state) => {
-            if (state.bmr && state.activityLevel) {
-                let activityFactor = 1;
-                switch (state.activityLevel) {
-                    case 'LOW':
-                        activityFactor = 1.2;
-                        break;
-                    case 'LIGHT':
-                        activityFactor = 1.375;
-                        break;
-                    case 'MEDIUM':
-                        activityFactor = 1.725;
-                        break;
-                    case 'HIGH':
-                        activityFactor = 1.9;
-                        break;
-                }
-                state.activityMetabolism = state.bmr * activityFactor;
-            }
+  name: "accountInfo",
+  initialState: accountInitialState,
+  reducers: {
+    setEmail: (state, action: PayloadAction<string | null>) => {
+      state.email = action.payload;
+    },
+    setName: (state, action: PayloadAction<string | null>) => {
+      state.name = action.payload;
+    },
+    setHeight: (state, action: PayloadAction<number | null>) => {
+      state.height = action.payload;
+    },
+    setWeight: (state, action: PayloadAction<number | null>) => {
+      state.weight = action.payload;
+    },
+    setAge: (state, action: PayloadAction<number | null>) => {
+      state.age = action.payload;
+    },
+    setGender: (state, action: PayloadAction<string | null>) => {
+      state.gender = action.payload;
+    },
+    setActivityLevel: (state, action: PayloadAction<string | null>) => {
+      state.activityLevel = action.payload;
+    },
+    calculateBMR: (state) => {
+      if (state.height && state.weight && state.age && state.gender) {
+        if (state.gender === "MALE") {
+          state.bmr = 88.362 + 13.397 * state.weight + 4.799 * state.height - 5.677 * state.age;
+        } else if (state.gender === "FEMALE") {
+          state.bmr = 447.593 + 9.247 * state.weight + 3.098 * state.height - 4.33 * state.age;
         }
-    }
-
-})
+      }
+    },
+    calculateActivityMetabolism: (state) => {
+      if (state.bmr && state.activityLevel) {
+        let activityFactor = 1;
+        switch (state.activityLevel) {
+          case "LOW":
+            activityFactor = 1.2;
+            break;
+          case "LIGHT":
+            activityFactor = 1.375;
+            break;
+          case "MEDIUM":
+            activityFactor = 1.725;
+            break;
+          case "HIGH":
+            activityFactor = 1.9;
+            break;
+        }
+        state.activityMetabolism = state.bmr * activityFactor;
+      }
+    },
+  },
+});
 
 //action을 export
 export const {
-    setEmail,
-    setName,
-    setHeight,
-    setWeight,
-    setAge,
-    setGender,
-    setActivityLevel,
-    calculateBMR,
-    calculateActivityMetabolism
+  setEmail,
+  setName,
+  setHeight,
+  setWeight,
+  setAge,
+  setGender,
+  setActivityLevel,
+  calculateBMR,
+  calculateActivityMetabolism,
 } = accountInfoSlice.actions;
 
 export default accountInfoSlice.reducer;
